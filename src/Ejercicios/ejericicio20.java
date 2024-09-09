eeae37391fbcbb0775c4167a30ea4243c6eadc3d
EduMoragaTsoft <eduardo.moraga@tsoftglobal.com>
2024-09-09 16:56:10 -0300

    cambio prueba

--- src/Ejercicios/ejericicio20.java
+++ src/Ejercicios/ejericicio20.java
@@ -1,5 +1,7 @@
 package Ejercicios;
 
+import java.util.Scanner;
+
 public class ejericicio20 {
     /*
     Determina si el número a = 153 es un número Armstrong.
@@ -7,7 +9,28 @@ public class ejericicio20 {
     la potencia del número de dígitos. Para 153, sería  1^3+5^3+3^3=153
      */
     public static void main(String[] args) {
-        int a = 153;
 
+        int num1 = 153;
+        int num2 = 153;
+        int num3 = 153;
+        double cifra = 0;
+        double suma = 0;
+        int contDig = 0;
+        while (num1 >= 1) {
+            contDig++;
+            num1/=10;
+        }
+
+        while (num2 != 0) {           //mientras a n le queden cifras
+            cifra = Math.pow(num2 % 10,contDig);        //obtenemos la ultima cifra del número
+            suma = suma + cifra;   //sumamos la última cifra de n
+            num2 = num2 / 10;            //le quitamos a n la última cifra
+        }
+        System.out.println(suma);
+        if (suma == num3) {
+            System.out.println("Es un número Armstrong");
+        }else {
+            System.out.println("No es un número Armstrong");
+        }
     }
 }
--- src/Ejercicios/ejericicio21.java
+++ src/Ejercicios/ejericicio21.java
@@ -8,5 +8,40 @@ public class ejericicio21 {
      */
     public static void main(String[] args) {
 
+        System.out.println(raizCuadrada(49));
+    }
+
+
+    public static int raizCuadrada (int numero) {
+        if (numero < 0) {
+            throw new ArithmeticException("La raíz cuadrada no está definida para números negativos.");
+        }
+
+        if (numero == 0 || numero == 1) {
+            return numero;
+        }
+
+        int a = 0;
+        int b = numero;
+        int mitad;
+
+        while (a <= numero) {
+            mitad  = (a + b) >> 1;
+
+            if (numero / mitad < mitad) {
+                b = mitad - 1;
+            } else {
+                if (numero / (mitad + 1) <= mitad) {
+                    return mitad;
+                }
+
+                a = mitad + 1;
+            }
+        }
+
+        return a;
     }
 }
+
+
+
